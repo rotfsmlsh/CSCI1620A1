0d41bdc16db8d14f0072b9fe0d8cf0918b9d2bf6
danwoc <danwoc@gmail.com>
2018-04-24 19:42:24 -0500

    commit all code

---
+++ SalaryEmployee.java
@@ -0,0 +1,101 @@
+/*  Name        : Daniel O'Connell
+ *  Class       : 1620-001
+ *  Program #   : 1
+ *  Due Date    : September 17, 2015
+ *  
+ *  Honor Pledge:  On my honor as a student of the University
+ *                of Nebraska at Omaha, I have neither given nor received
+ *                unauthorized help on this homework assignment.
+ *
+ * NAME: Daniel O'Connell
+ * NUID: 12992765
+ * EMAIL: danwoc@gmail.com	
+ * Partners:  none
+ *
+ * This program creates a simple class hierarchy building upon the Employee
+ * example discussed in class. This program simulates an employee management 
+ * system that might be used by restaurants or similarly structured 
+ * organizations.
+ */
+
+package employeeType.subTypes;
+import employeeType.employee.Employee;
+
+public class SalaryEmployee extends Employee 
+{
+    private double salary;
+
+    /*  Method Name   : <<SalaryEmployee>>
+     *  Parameters    : Strings for first name and last name
+     *                  Characters for gender and middle initial
+     *                  Integer for employee number
+     *                  Boolean for full time or part time
+     *                  Double for annual salary
+     *  Return Values : N/A.
+     *  Partners      : None.
+     *  Description   : Constructor method for SalaryEmployee.java, accepts all
+     *                  that an Employee requires as well as a double for salary.
+     */
+    public SalaryEmployee(String fn, String ln, char m, char g, int empNum, boolean ft, double s)
+    {
+        super(fn, ln, m, g, empNum, ft);
+        salary = s;
+    }
+
+    /*  Method Name   : toString
+     *  Parameters    : None.
+     *  Return Values : Formatted String.
+     *  Partners      : None.
+     *  Description   : Overrides the Employee toString method to additionally list
+     *                  annual salary. 
+     */
+    @Override
+    public String toString()
+    {
+        return String.format("%sSalary: %.2f\n", super.toString(), salary);
+    }
+
+    /*  Method Name   : calculateWeeklyPay
+     *  Parameters    : None.
+     *  Return Values : Double for amount of weekly pay.
+     *  Partners      : None.
+     *  Description   : Returns the weekly pay of a salary employee. 
+     */
+    public double calculateWeeklyPay()
+    {
+        return salary/52;
+    }
+
+    /*  Method Name   : annualRaise
+     *  Parameters    : None.
+     *  Return Values : None.
+     *  Partners      : None.
+     *  Description   : Applies an annual raise of six percent to the 
+     *                  salary of a salary employee. Rounds down to the 
+     *                  nearest hundredth. 
+     */
+    public void annualRaise()
+    {
+        salary *= 1.06;
+    }
+
+    /*  Method Name   : holidayBonus
+     *  Parameters    : None.
+     *  Return Values : Double for amount of holiday bonus to be paid.
+     *  Partners      : None.
+     *  Description   : Calculates and returns the amount of money to be 
+     *                  paid to a salary employee for thier holiday bonus. 
+     */
+    public double holidayBonus()
+    {
+        return salary * .03;
+    }
+
+    /*  Method Name   : resetWeek
+     *  Parameters    : None.
+     *  Return Values : None.
+     *  Partners      : None.
+     *  Description   : No function. 
+     */
+    public void resetWeek(){}
+}
